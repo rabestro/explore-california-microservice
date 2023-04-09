c6c06dd7ca9fe809cc3c323edd737d3e19d31958
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-09 14:44:01 +0300

    Add support for HAL Explorer closes #15

--- src/main/java/com/epam/engx/explorecalifornia/repository/TourRepository.java
+++ src/main/java/com/epam/engx/explorecalifornia/repository/TourRepository.java
@@ -2,6 +2,32 @@ package com.epam.engx.explorecalifornia.repository;
 
 import com.epam.engx.explorecalifornia.domain.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer>, CrudRepository<Tour, Integer> {
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(Integer integer);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour tour);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
