15d9b77b5b60624e603def99e2effdba44c53154
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-10 11:37:25 +0300

    feat: Add TourRatingController closes #26

---
+++ src/main/java/com/epam/engx/explorecalifornia/controller/TourRatingController.java
@@ -0,0 +1,67 @@
+package com.epam.engx.explorecalifornia.controller;
+
+import com.epam.engx.explorecalifornia.domain.Tour;
+import com.epam.engx.explorecalifornia.domain.TourRating;
+import com.epam.engx.explorecalifornia.domain.TourRatingPk;
+import com.epam.engx.explorecalifornia.dto.Rating;
+import com.epam.engx.explorecalifornia.repository.TourRatingRepository;
+import com.epam.engx.explorecalifornia.repository.TourRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.NoSuchElementException;
+
+
+@RestController
+@RequestMapping(path = "/tours/{tourId}/ratings")
+@RequiredArgsConstructor
+public class TourRatingController {
+    private final TourRatingRepository tourRatingRepository;
+    private final TourRepository tourRepository;
+
+    /**
+     * Create a Tour Rating.
+     *
+     * @param tourId tour identifier
+     * @param rating rating data transfer object
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated Rating rating) {
+        var tour = verifyTour(tourId);
+        tourRatingRepository.save(new TourRating(
+            new TourRatingPk(tour, rating.customerId()), rating.score(), rating.comment()));
+    }
+
+    /**
+     * Verify and return the Tour given a tourId.
+     *
+     * @param tourId tour identifier
+     * @return the found Tour
+     * @throws NoSuchElementException if no Tour found.
+     */
+    private Tour verifyTour(int tourId) throws NoSuchElementException {
+        return tourRepository.findById(tourId).orElseThrow(() ->
+            new NoSuchElementException("Tour does not exist " + tourId));
+    }
+
+    /**
+     * Exception handler if NoSuchElementException is thrown in this Controller
+     *
+     * @param ex exception
+     * @return Error message String.
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public String return400(NoSuchElementException ex) {
+        return ex.getMessage();
+    }
+}
