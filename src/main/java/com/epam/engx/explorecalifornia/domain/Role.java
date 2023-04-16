54890ef3864aaf68e0c88e44ef2186612c92a5c2
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-04-16 16:32:11 +0300

    feat: Add Security Role entity closes #94

---
+++ src/main/java/com/epam/engx/explorecalifornia/domain/Role.java
@@ -0,0 +1,34 @@
+package com.epam.engx.explorecalifornia.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serial;
+
+@Data
+@Entity
+@Table(name = "security_role")
+public class Role implements GrantedAuthority {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="role_name")
+    private String roleName;
+
+    @Column(name="description")
+    private String description;
+
+    @Override
+    public String getAuthority() {
+        return roleName;
+    }
+}
